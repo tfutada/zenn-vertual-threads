0d83205621ba8b1bcfe7e2c8276a57eaa69e05a3
tfutada <tafutada@gmail.com>
2023-12-09 16:34:14 +0900

    Add Gradle wrapper and initial Spring Boot structure
    This commit introduces the typical initial Spring Boot Game structure including Gradle wrapper and build.gradle file. Additionally, basic application properties file, application main class, and a placeholder test class have been added.

---
+++ src/main/java/jp/tf/zennjdk21vthread/ZennJdk21VthreadApplication.java
@@ -0,0 +1,13 @@
+package jp.tf.zennjdk21vthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZennJdk21VthreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZennJdk21VthreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/tf/zennjdk21vthread/ZennJdk21VthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.tf.zennjdk21vthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZennJdk21VthreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
