338c03c76a74a0110c8b1a101d6887398f2da9b0
tfutada <tafutada@gmail.com>
2023-12-09 17:05:30 +0900

    Add new Vt1 class and modify ZennJdk21VthreadApplication class
    Created a new class named Vt1 with multithreading capabilities, updates in build.gradle for Gradle wrapper, and made modifications in the existing ZennJdk21VthreadApplication class. The changes ensure streamlined execution of tasks using configurable threads and enhanced logging.

---
+++ src/main/java/jp/tf/zennjdk21vthread/Vt1.java
@@ -0,0 +1,93 @@
+package jp.tf.zennjdk21vthread;
+
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+
+@SpringBootApplication
+public class Vt1 {
+    private static final Logger log = LoggerFactory.getLogger(Vt1.class);
+    //    private static final Lock lock = new ReentrantLock();
+    private static final ThreadLocal<String> context = new ThreadLocal<>();
+
+    // synchronizedで同期処理にしてみる
+    @SneakyThrows
+    private static void sleep(Duration duration) {
+        Thread.sleep(duration);  // unmount(スレッドを譲る)
+        log.info("!!! carrier thread is  {} ", Thread.currentThread());
+    }
+
+    @SneakyThrows
+    private static void myTask(Duration duration) {
+        log.info("『  {} : {} ", context.get(), Thread.currentThread());
+        sleep(duration);  // unmount
+//        fibonacciRecursive(47);
+//        httpClient();
+        log.info("』 {} ", Thread.currentThread());
+    }
+
+    private static long fibonacciRecursive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+    @SneakyThrows
+    private static void httpClient() {
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("https://pokeapi.co/api/v2/pokemon/ditto"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        log.info("Status Code: {}", response.statusCode());
+    }
+
+    @SneakyThrows
+    static void runMyTasks() {
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+
+            List<Future<Integer>> futures = new ArrayList<>();
+
+            var numCore = Runtime.getRuntime().availableProcessors();
+
+            IntStream.range(0, numCore + 1).forEach(i -> {
+                // Submit a task and get a Future
+                Future<Integer> future = executor.submit(() -> {
+                    context.set("task-" + i);
+                    myTask(Duration.ofSeconds(5));
+                    log.info("done task #{}", i);
+                    return i;
+                });
+                futures.add(future);
+            });
+
+            log.info("submitted all tasks");
+
+            // Wait for all tasks to complete
+            for (var future : futures) {
+                log.info("result: {}", String.valueOf(future.get()));
+            }
+        }
+    }
+
+    //
+    public static void main(String[] args) {
+        runMyTasks();
+    }
+}
--- src/main/java/jp/tf/zennjdk21vthread/ZennJdk21VthreadApplication.java
+++ src/main/java/jp/tf/zennjdk21vthread/ZennJdk21VthreadApplication.java
@@ -1,13 +1,15 @@
 package jp.tf.zennjdk21vthread;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ZennJdk21VthreadApplication {
+    private static final Logger log = LoggerFactory.getLogger(ZennJdk21VthreadApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(ZennJdk21VthreadApplication.class, args);
+        log.info("!!! started from ZennJdk21VthreadApplication...");
     }
-
 }
